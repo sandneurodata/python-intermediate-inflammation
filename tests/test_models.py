13329a6f5d3f3c7ecb7bee6c7684f85ecb1c380e
Sand Data <sandlefort250880@gmail.com>
2023-05-31 06:19:11 +0200

    Implement data normalisation

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,7 +7,9 @@ import pytest
 
 @pytest.mark.parametrize(
     "test, expected",
-    [([[0, 0], [0, 0], [0, 0]], [0, 0]), ([[1, 2], [3, 4], [5, 6]], [3, 4])],
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]), ([[1, 2], [3, 4], [5, 6]], [3, 4])
+    ],
 )
 def test_daily_mean(test, expected):
     """Test that mean function works for an array of zeros."""
@@ -44,6 +46,24 @@ def test_daily_min(test, expected):
     from inflammation.models import daily_min
 
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0,0,0],[0,0,0],[0,0,0]],[[0,0,0],[0,0,0],[0,0,0]]),
+        ([[1,1,1],[1,1,1],[1,1,1]],[[1,1,1],[1,1,1],[1,1,1]]),
+        ([[1,1,1],[1,1,1],[np.nan,1,1]],[[1,1,1],[1,1,1],[0,1,1]]),
+        ([[1,-2,3],[4,5,6],[7,8,9]],[[0.33,0,1],[0.67,0.83,1],[0.78,0.89,1]]),
+        ([[1,2,3],[np.nan,5,6],[7,8,9]],[[0.33,0.67,1],[0,0.83,1],[0.78,0.89,1]]),
+        ([[1,2,3],[4,5,6],[7,8,9]],[[0.33,0.67,1],[0.67,0.83,1],[0.78,0.89,1]])
+    ]
+)
+def test_patient_normalise(test, expected):
+    """Test normalisation works for arrays for one and positive integers.
+        Assumption that test accuracy of two decimal places is sufficient."""
+    from inflammation.models import patient_normalise
+    npt.assert_almost_equal(patient_normalise(np.array(test)),np.array(expected),decimal=2)
 
 
 def test_daily_min_string():
